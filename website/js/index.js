b0e0625b4c832af96fa02351a848a81902737b3d
Jackson Morrow <77409000+jacksonmorrow@users.noreply.github.com>
2023-08-06 13:40:18 -0700

    Delete website directory

--- website/js/index.js
+++
@@ -1,9 +0,0 @@
-//Javascr code
-const counter = document.querySelector(".counter-number");
-async function updateCounter() {
-    let response = await fetch("https://qmpu3xzu4ujexxbxjpkedpegyi0qygvx.lambda-url.us-west-1.on.aws/");
-    let data = await response.json();
-    counter.innerHTML = `Views: ${data}`;
-}
-
-updateCounter();
