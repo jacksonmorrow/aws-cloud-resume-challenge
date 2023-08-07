f96333d5bfb1312d4428f75adee7f0e75d402db4
jackson <morrowj3@wit.edu>
2023-08-06 17:51:45 -0700

    first commit

---
+++ website/js/index.js
@@ -0,0 +1,8 @@
+//Javascript code
+const counter = document.querySelector(".counter-number");
+async function updateCounter(){
+    let response = await fetch("https://qmpu3xzu4ujexxbxjpkedpegyi0qygvx.lambda-url.us-west-1.on.aws/");
+    let data = await response.json();
+    counter.innerHTML = `Views: ${data}`;
+}
+updateCounter();
